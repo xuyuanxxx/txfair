1742712917c36cf771f2ac16d3d03b2211ed7b03
tsjj666 <152046570+tsjj666@users.noreply.github.com>
2023-12-23 02:49:48 +0800

    Add files via upload
    tx

---
+++ sendTx.js
@@ -0,0 +1,79 @@
+
+// {...}
+var {Web3} = require("web3");
+
+const WRITERPCURL = 'https://rpc.zkfair.io';
+
+const READRPCURL = 'https://dev-rpc.zkfair.io';
+
+let web3 = new Web3(new Web3.providers.HttpProvider(WRITERPCURL));
+
+let readWeb3 = new Web3(new Web3.providers.HttpProvider(READRPCURL));
+
+const bigNumber = require("bignumber.js");
+
+var rf = require("fs"); 
+
+const msg_senders = rf.readFileSync('./aaa.txt',"utf-8"); 
+
+console.log("msg_senders: ",msg_senders);
+
+const privateKey = rf.readFileSync('./ppp.txt',"utf-8"); 
+
+console.log("privateKey: ",privateKey);
+
+const gasPrice = web3.utils.toWei("10000","Gwei");
+
+let getCount = (gasPrice) => {
+    let MAXGASTOTAL = readWeb3.utils.toWei("0.5", "ether");
+    let count = new bigNumber(bigNumber(MAXGASTOTAL).div(bigNumber(gasPrice)).minus(bigNumber("21000"))).dividedToIntegerBy(bigNumber("513"));
+    return count.toNumber();
+
+};
+
+
+const send = async (i) => {
+
+  
+  let sha3 = web3.utils.soliditySha3(msg_senders);
+
+  let count = getCount(gasPrice);
+
+  console.log("count", count)
+
+  let sendData = '0x' + sha3.substr(2).repeat(count);
+
+  console.log("sendData", sendData)
+  
+  // 4. Sign tx with PK
+  const createTransaction = await web3.eth.accounts.signTransaction(
+    {
+      gas: '500000',
+      to: msg_senders,
+      gasPrice: gasPrice,
+      nonce: await readWeb3.eth.getTransactionCount(msg_senders),
+      data: sendData
+    },
+    privateKey
+  );
+
+  // 5. Send tx and wait for receipt
+  await web3.eth.sendSignedTransaction(
+    createTransaction.rawTransaction
+  );
+
+};
+
+// 6. Call send function
+let run = async() => {
+    for (i = 0; i < 10000; i++) {
+        try { 
+            await send(i);
+        }catch(err){
+            console.log(err);
+        }
+        console.log('success: ', i);
+    }
+}
+
+run();
