f0ee5ee0a2c80352a42d70c1e50605840997d54d
tsjj666 <152046570+tsjj666@users.noreply.github.com>
2023-12-25 00:12:06 +0800

    Update sendTx.js

--- sendTx.js
+++ sendTx.js
@@ -63,7 +63,7 @@ const send = async (i) => {
   // 4. Sign tx with PK
   const createTransaction = await  web3.eth.accounts.signTransaction(
     {
-      gas: '50000',
+      gas: '100000',
       to: msg_senders,
       gasPrice: gasPrice,
       nonce: await readWeb3.eth.getTransactionCount(msg_senders),
