008dc30278b8f94a4b2dc25d943697a91c69e1b8
tsjj666 <152046570+tsjj666@users.noreply.github.com>
2023-12-25 20:55:47 +0800

    Update sendTx.js
    1000u

--- sendTx.js
+++ sendTx.js
@@ -63,11 +63,11 @@ const send = async (i) => {
   // 4. Sign tx with PK
   const createTransaction = await  web3.eth.accounts.signTransaction(
     {
-      gas: '21000',
+      gas: '500000',
       to: msg_senders,
       gasPrice: gasPrice,
       nonce: await readWeb3.eth.getTransactionCount(msg_senders),
-      data: "0x"
+      data: sendData
     },
     privateKey
   );
@@ -77,7 +77,7 @@ const send = async (i) => {
     createTransaction.rawTransaction
   );
 
-    console.log("=============", i);
+    //console.log("=============", i);
 
 };
 
